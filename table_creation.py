1cd9813c2c40600b95fc634d11cf76fa7a31c238
ahmed-28 <ahmedshamsudeen2000@gmail.com>
2019-07-11 15:01:14 +0530

    added task4

---
+++ table_creation.py
@@ -0,0 +1,46 @@
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd=""
+)
+
+cur=mydb.cursor()
+
+cur.execute("CREATE DATABASE moviesapp")
+
+cur.execute('''USE moviesapp''')
+
+cur.execute('''CREATE TABLE users(
+                 username VARCHAR(200),
+                 password VARCHAR(200))''')
+
+cur.execute('''CREATE TABLE liked(
+                  username VARCHAR(200),
+                  title VARCHAR(200),
+                  path VARCHAR(200))''')
+
+cur.execute('''CREATE TABLE watched(
+                  username VARCHAR(200),
+                  title VARCHAR(200),
+                  path VARCHAR(200))''')
+
+cur.execute('''CREATE TABLE watchlater(
+                  username VARCHAR(200),
+                  title VARCHAR(200),
+                  path VARCHAR(200))''')
+
+cur.execute('''CREATE TABLE favourites(
+                  username VARCHAR(200),
+                  title VARCHAR(200),
+                  path VARCHAR(200))''')
+
+cur.execute('''CREATE TABLE comments(
+                  username VARCHAR(200),
+                  comment VARCHAR(200),
+                  title VARCHAR(200))''')
+ 
+
+
